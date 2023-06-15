83bcf4db171b2e1d19a21939f600493aebd979a3
langochai <langochai11121998@gmail.com>
2023-06-15 10:45:13 +0700

    create

---
+++ index.ts
@@ -0,0 +1,36 @@
+
+import express from "express";
+
+import bodyParser from "body-parser";
+
+import mongoose from "mongoose";
+import bookRoutes from "./src/router/book.router";
+
+
+
+const PORT = 8000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.set('views', './src/views');
+
+const DB_URL = 'mongodb://127.0.0.1:27017/blog';
+
+mongoose.connect(DB_URL)
+
+    .then(() => console.log('DB Connected!'))
+
+    .catch(error => console.log('DB connection error:', error.message));
+
+app.use(bodyParser.json());
+app.use('/book', bookRoutes);
+
+
+app.listen(PORT, () => {
+
+    console.log("App running on port: " + PORT)
+
+})
+
